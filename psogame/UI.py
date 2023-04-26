b54dc750fcd0aa41029580b99045c27b51cd1722
matdelar <70250770+matdelar@users.noreply.github.com>
2023-04-26 19:14:08 -0300

    Add files via upload

--- psogame/UI.py
+++ psogame/UI.py
@@ -2,43 +2,40 @@ import pygame
 import SceneManager
 
 class Button:
-    def __init__(self,func,screen,pos,size,text,tcolor,slcolor,srcolor) -> None:
+    def __init__(self,func,screen,pos,size,text,textColor,color) -> None:
         self.screen = screen
         self.pos = pos
         self.size = size
         self.text = text
-        self.tcolor = tcolor
-        self.slcolor = slcolor
-        self.srcolor = srcolor
+        self.tcolor = textColor
+        self.color = color
         self.active = False
         self.func = func
         self.rect = pygame.Rect(pos,size)
         pygame.font.init()
         self.font = pygame.font.Font("assets/invasion2000.ttf",self.size[1])
-
-    def gradientRect(self):
-        fadeRect = pygame.Surface( ( 2, 2 ) )                                   
-        fadeRect.set_at((0,0),self.slcolor)
-        fadeRect.set_at((0,1),self.slcolor)
-        fadeRect.set_at((1,0),self.srcolor)
-        fadeRect.set_at((1,1),self.srcolor)
-        fadeRect = pygame.transform.smoothscale( fadeRect, ( self.rect.width, self.rect.height ) )  # stretch!
-        self.screen.blit( fadeRect, self.rect )   
     
+    def selected_draw(self):
+        pygame.draw.rect(self.screen,self.color,self.rect)
+
     def draw(self):
-        self.gradientRect() if self.active else True
+        self.selected_draw() if self.active else True
         text1 = self.font.render(self.text, False, self.tcolor)
         self.screen.blit(text1, self.pos)
     
     def get_func(self):
         return self.func
     
+    def mouse_isOver(self):
+        m = pygame.mouse.get_pos()
+        return self.rect.collidepoint(m[0], m[1])
+    
     def set_active(self, nstate = None):
         if nstate == None:
             self.active = not self.active
         else:
             self.active = nstate
-
+            
 class Transition:
     def __init__(self,screen,auto = True,timeins = 2,spacing=24) -> None:
         self.screen = screen
